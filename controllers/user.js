57ce5ee39ac879c57d6e9658cb6b2d1757735422
arturo <aarturock92@gmail.com>
2017-06-10 23:05:51 -0500

    Create User

--- controllers/user.js
+++ controllers/user.js
@@ -10,14 +10,57 @@ function pruebas(req, res){
 
 function saveUser(req, res){
     var user = new User(),
-        params = req.body;
-    console.log('params', params)
+        params = req.body,
+        message = [];
 
-    user.name = params.name;
-    user.surname = params.surname;
-    user.email = params.email;
     user.role = 'ROLE_USER';
     user.image = 'null';
+
+    if(params.name){
+       user.name = params.name;
+    }else{
+       message.push('Introduce el Nombre')
+    }
+
+    if(params.surname){
+        user.surname = params.name;
+    }else{
+        message.push('Introduce el Usuario')
+    }
+
+    if(params.email){
+        user.email = params.email;
+    }else{
+        message.push('Introduce el email')        
+    }
+
+    if(params.password){
+        bcrypt.hash(params.password, null, null, function(err, hash){
+            user.password = hash;
+            console.log("user.password", user);
+            if(user.name != null && user.surname != null && user.email != null){
+                user.save((err, userStored) => {
+                    if(err){
+                        message.push('Error al guardar el usuario')
+                    }else{
+                        if(!userStored){
+                            res.status(404).send('No se ha registrado el usuario')
+                        }else{
+                            res.status(200).send({user: userStored})
+                        }
+                    }
+                });
+            }else{
+                message.push('Rellene todos los campos');
+            }
+        });        
+    }else{
+        message.push('Introduce la contraseña')        
+    }
+
+    if(message.length > 0){
+        res.status(500).send(message);
+    } 
 }
 
 module.exports = {
